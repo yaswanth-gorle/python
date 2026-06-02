efd462c5acf77403fd3b71c6ce0ddbe9bc05c2ec
yashwanth gorle <yashwanth.gorle117@gmail.com>
2026-06-02 08:39:30 +0530

    updated output.py file

---
+++ output.py
@@ -0,0 +1,21 @@
+# a = int(input("enter value of a :"))
+# b = int(input("enter value of b :"))
+# print(a,b,a,b,sep = ",", end = "ended here.\n")# here sep is used to seperate the outputs of every variable and end is used to print the given statement at the end of the print statement
+# # here seperate which seberate using , in the output
+
+
+# name = input("enter name :")
+# print("hello",name,sep = "," ,end = "!\n")  
+
+# c = int(input("Enter the value of c :"))
+# print("you entered ",c,sep = ":")
+
+# d = float(input("enter the value of d :"))
+# print("value of PI:",d,sep=" ")
+
+e = input("enter value of e : ")
+x,y,z = e.split(" ")
+sum = int(x) + int(y) + int(z)
+print(sum)
+
+
